6ba637d63dbb44004dcfa9e9fb5592cc3ba1c0be
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-01-29 14:11:22 +0100

    adding play routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { TodosComponent } from './components/todos/todos.component';
 import {AboutComponent} from './components/pages/about/about.component';
+import {PlayComponent} from './components/pages/play/play/play.component';
 
 const routes: Routes = [
   { path: '', component: TodosComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'play', component: PlayComponent }
 ];
 
 
