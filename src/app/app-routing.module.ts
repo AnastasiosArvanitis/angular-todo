683c784e30481d024164d52237be8ff89475015d
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-02-01 13:47:31 +0100

    adding film routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { TodosComponent } from './components/todos/todos.component';
 import {AboutComponent} from './components/pages/about/about.component';
 import {PlayComponent} from './components/pages/play/play/play.component';
+import {SearchFilmComponent} from './components/searchFilm/search-film/search-film.component';
 
 const routes: Routes = [
   { path: '', component: TodosComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'play', component: PlayComponent }
+  { path: 'play', component: PlayComponent },
+  { path: 'searchFilm', component: SearchFilmComponent }
 ];
 
 
